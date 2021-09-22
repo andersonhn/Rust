8550f99fc8d84dcdec38b3ea306b778bb324cc77
anderson.hermann@orsegups.com.br <andersonHermannNotholt@bitbucket.org>
2021-09-22 15:49:08 -0300

    Rust:    hello_world.rs - just initializing with rust and config IntellJ    guessing_game - A simple tutorial and the understanding of the simple concepts.

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,40 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+// let test = 5 is a immutable variable
+// let mut test = 5 is a mutable variable
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        println!("You guessed: {}", guess);
+
+        // In rust you can recreate a variable with same name, this feature is called Shadowing and is very useful in type changes.
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+
+}
